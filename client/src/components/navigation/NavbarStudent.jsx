4f55cc84fa54d650681dd888d96e71f9a6323cc4
Jey Nang Gogoi <156996004+Jeynang2024@users.noreply.github.com>
2025-07-08 14:05:35 +0530

    Update NavbarStudent.jsx

--- client/src/components/navigation/NavbarStudent.jsx
+++ client/src/components/navigation/NavbarStudent.jsx
@@ -1,6 +1,7 @@
 import  { useEffect } from "react";
 import { useCookies } from 'react-cookie';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate,Link } from 'react-router-dom';
+
 const NavbarStudent = () => {
   const [cookies, , removeCookie] = useCookies(['authToken']);
   const navigate = useNavigate();
